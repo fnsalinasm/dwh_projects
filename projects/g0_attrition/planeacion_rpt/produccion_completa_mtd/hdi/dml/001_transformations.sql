5c0f1c7d6cf0a23f1047ff7dbc6ddf31cc30d1d7
andres bejarano <abejarano770@gmail.com>
2024-10-07 13:41:49 -0500

    modificacion SQL produccion completa MTD

--- projects/g0_attrition/planeacion_rpt/produccion_completa_mtd/hdi/dml/001_transformations.sql
+++ projects/g0_attrition/planeacion_rpt/produccion_completa_mtd/hdi/dml/001_transformations.sql
@@ -4,6 +4,9 @@ Autor: Fabio Salinas
 Email: fabio.salinas@hdi.com.co
 Fecha de creación de script: 06 Octubre de 2024 (2024-10-06)
 Descripción del script: Query de ejecución de transformaciónes de datos
+
+ - start_date:	{start_date}
+ - end_date:	{end_date}
 */
 
 -- Query de ejecución dummy
