23e2c542c5ea765c8e05507448c74216826b0be5
fabio <fabio.salinas@hdi.com.co>
2024-10-08 00:41:12 -0500

    Agregados ajustes en extracción de PRODUCCION_COMPLETA

--- projects/g0_attrition/planeacion_rpt/produccion_completa/hdi/validaciones/001_validations.sql
+++ projects/g0_attrition/planeacion_rpt/produccion_completa/hdi/validaciones/001_validations.sql
@@ -4,6 +4,9 @@ Autor: Fabio Salinas
 Email: fabio.salinas@hdi.com.co
 Fecha de creación de script: 06 Octubre de 2024 (2024-10-06)
 Descripción del script: Query de validaciones de datos para la generación de un archivos csv
+Variables de remplazo:
+ - start_date:	{start_date}
+ - end_date:	{end_date}
 */
 
 -- Query de ejecución dummy
